fd58f915000d524d5988da4d29cf04bd7c756ae4
Samuel Kim <sam.yh.kim@gmail.com>
2020-05-26 16:05:37 -0700

    Building out error handling in auth service. Good place to commit.

---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,35 @@
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+
+const router = express.Router();
+
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    // append errors to req object
+    const errors = validationResult(req);
+
+    // return error back to client if errors exist
+    if (!errors.isEmpty()) {
+      throw new Error("Invalid email or password");
+    }
+
+    const { email, password } = req.body;
+
+    console.log("Creating a user...");
+    throw new Error("Error connecting to database");
+
+    res.send({});
+
+    // new User({email, password})
+  }
+);
+
+export { router as signupRouter };
